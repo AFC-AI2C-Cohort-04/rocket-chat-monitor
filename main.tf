e43d888146342985160501b119336ca55ac47ff8
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2023-10-07 09:32:07 -0400

    Debugging Azure pipeline

---
+++ main.tf
@@ -0,0 +1,44 @@
+provider "azurerm" {
+  features {}
+}
+
+variable "region" {
+  type        = string
+  default     = "eastus"
+}
+
+variable "resource_group" {
+  type        = string
+  default     = "rocket-chat-containers-resource-group"
+}
+
+resource "azurerm_resource_group" "resource_group" {
+  location = var.region
+  name     = var.resource_group
+}
+
+resource "azurerm_container_registry" "container_registry" {
+  name                     = "ai2ccohort04rcmregistry.azurecr.io"
+  location                 = var.region
+  resource_group_name      = var.resource_group
+  sku                      = "Standard"
+  admin_enabled            = true
+}
+
+resource "azurerm_container_group" "container_group" {
+  name                = "rocket-chat-container-group"
+  location            = var.region
+  resource_group_name = var.resource_group
+  os_type             = "Linux"
+
+  container {
+    name   = "elasticsearch"
+    image  = "${azurerm_container_registry.container_registry.login_server}/elasticsearch:39"
+    cpu    = "0.5"
+    memory = "1.5"
+  }
+
+  tags = {
+    environment = "testing"
+  }
+}
